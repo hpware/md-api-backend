505a07475ef02ebfc96e01aa27a3e7e7b6c5aa58
吳元皓 <hpware3@gmail.com>
2025-02-20 00:33:06 +0800

    Add Supabase integration for slug generation and update dependencies

--- components/genSlug.ts
+++ components/genSlug.ts
@@ -1,8 +1,26 @@
-let slug = ""
+import * as db from "@supabase/supabase-js";
+const supabase = db.createClient(
+    process.env.supabase_domain,
+    process.env.supabase_key
+)
+
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-export default function(length) {
+export default async function(length) {
     try {
-        
+        let slug = ""
+        for (let times = 0; times < length; times++) {
+            slug += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+        const check = await supabase.from("slugs").select("*").eq("slug","asd");
+        if (check.data = []) {
+            return slug;
+        } else {
+            slug += characters.charAt(Math.floor(Math.random() * characters.length)) 
+            const check2= await supabase.from("slugs").select("*").eq("slug","asd");
+            if (check2.data = []) {
+                return slug;
+            }  
+        }
     } catch (e) {
         console.log(e);
     }
