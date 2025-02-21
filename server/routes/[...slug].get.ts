bedfc82971460cf03f8a63bf7ddfdeb083058947
吳元皓 <hpware3@gmail.com>
2025-02-21 20:42:19 +0800

    deploy to vercel.

--- server/routes/[...slug].get.ts
+++ server/routes/[...slug].get.ts
@@ -1,9 +1,20 @@
 import supabase from "../../components/supabase";
-
+import { v4 as uuidv4 } from "uuid";
 export default defineEventHandler(async (event) => {
   try {
     setHeader(event, "Content-Type", "text/markdown")
-    const slug = getRouterParam(event, "slug");
+    const slugorg = getRouterParam(event, "slug").replace("/", ",");
+    const etag = event.headers.get("etag");
+    const date = event.headers.get("date");
+    console.log(slugorg)
+    let slug = ""
+    let slugboolean = false;
+    if (slugorg.includes(",json") || slugorg.includes("json,")) {
+        slug = slugorg.replace("json", "").replace(",", "");
+        slugboolean = true;
+    } else {
+        slug = slugorg
+    }
     const getMD = await supabase
       .from("markdown")
       .select("*")
@@ -11,10 +22,23 @@ export default defineEventHandler(async (event) => {
       .maybeSingle();
       // Its just maybe single, I have no clue what does do.
       const data = getMD;
-      if (!data.data || data.data === null) {
-        return "Content not found"
+      if (slugboolean = true) {
+        if (!data.data || data.data === null) {
+            return data
+          } else {
+            return {
+                slug: data.data.slug,
+                content: data.data.content,
+                date_created: data.data.date_created,
+                event: etag,
+            }
+          }
       } else {
-        return data.data.content
+        if (!data.data || data.data === null) {
+            return "## Content not found"
+          } else {
+            return data.data.content
+          }
       }
   } catch (e) {
     console.log(e);
