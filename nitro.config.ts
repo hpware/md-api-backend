340d0c77e313c8c7e73e6d41d84ff446f1eef46f
吳元皓 <hpware3@gmail.com>
2025-02-25 09:46:40 +0800

    Remove OpenAPI metadata from nitro configuration

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,11 +2,4 @@
 export default defineNitroConfig({
   srcDir: "app",
   compatibilityDate: "2025-02-19",
-  openAPI: {
-    meta: {
-      title: 'Markdown API APP',
-      description: '',
-      version: '0.1.0'
-    }
-  },
 });
